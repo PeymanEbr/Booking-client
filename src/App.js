d41522ce33624ccdecdba7ae8600421b6ddb9021
DESKTOP-VO9MLPP\peyman <peyman.ebr68@gmail.com>
2023-09-28 12:04:13 +0330

    booking client init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./booking/Home";
+import Login from "./auth/Login";
+import Registe from "./auth/Register";
+
+import TopNav from "./components/TopNav";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <TopNav />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Registe} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
