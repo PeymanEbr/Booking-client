1de2394d649a559fb798baf2fc3f43a2a16f577a
DESKTOP-VO9MLPP\peyman <peyman.ebr68@gmail.com>
2023-09-28 18:52:11 +0330

    next step

--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -1,5 +1,48 @@
+import { useState } from "react";
+import axios from 'axios';
+import RegisterForm from "../components/RegisterForm";
+
 const Register = () => {
-    return <div className="container-fluid h1 p-5 text-center">Register Page</div>
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            const res = await axios.post('http://localhost:8000/api/register',{
+            name,
+            email,
+            password
+        });
+        console.log(res);
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    return (
+        <>
+        <div className="container-fluid bg-secondary p-5 text-center">
+            <h1>Register</h1>
+        </div>
+        <div className="container">
+            <div className="tow">
+                <div className="col-md-6 offset-md-3">
+                    <RegisterForm
+                        handleSubmit={handleSubmit}
+                        name={name}
+                        setName={setName}
+                        email={email}
+                        setEmail={setEmail}
+                        password={password}
+                        setPassword={setPassword} 
+                    />
+                </div>
+            </div>
+        </div>
+        </>
+    );
 }
 
 export default Register;
---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,40 @@
+const RegisterForm = ({handleSubmit,name,setName,email,setEmail,password,setPassword}) => {
+    return(
+        <form onSubmit={handleSubmit} className="mt-3">
+            <div className="form-group mb-3">
+                <label className="form-lable">Your Name</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter your Name ..."
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </div>
+            <div className="form-group mb-3">
+                <label className="form-lable">Your Email</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter your Email ..."
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </div>
+            <div className="form-group mb-3">
+                <label className="form-lable">Your Password</label>
+                <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Enter your Email ..."
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+    );
+}
+
+export default RegisterForm;
